77c5359d4f858a7ab842af751b09606a21935f39
s2100637 <peter.knutas@edu.bc.fi>
2022-03-03 10:25:54 +0200

    phone-list working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PhoneDetailComponent } from './phone-detail/phone-detail.component';
+import { PhoneListComponent } from './phone-list/phone-list.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/phone-list' },
+  { path: 'phone-list', component: PhoneListComponent },
+  { path: 'phone-detail/:id', component: PhoneDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
